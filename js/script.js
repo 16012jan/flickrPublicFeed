cff58d9096c6eefb9f9d5ccfa0364f3e252619ab
igorTo <16012jan@gmail.com>
2016-07-14 14:37:59 +0300

    Add ability to filter and sort feeds

--- js/script.js
+++ js/script.js
@@ -1,56 +1,99 @@
 'use strict';
 
-	var flickrFeeds = {
-		feed: [],
-		flickrOptions: {
-			url: 'https://api.flickr.com/services/feeds/photos_public.gne?format=json&lang=en-us&tags=fmx',
-			tags: ''
-		}
-	};
-
-	if(head){
-	  head.removeChild('script');
+var flickrFeeds = {
+	feed: [],
+	flickrOptions: {
+		url: 'https://api.flickr.com/services/feeds/photos_public.gne?format=json&lang=en-us&tags=fmx',	
 	}
-	var script = document.createElement('script');
-  script.src = flickrFeeds.flickrOptions.url;
-  var head = document.head;
-  head.appendChild(script);
-
-	function jsonFlickrFeed(data){
-		var count = 0;
-	    flickrFeeds.feed = data.items.map(function( item ){
-	    	item.count = count++;
-	      return item;
-	    });
-	  drawFeed();
-	}
+};
+	
+function jsonFlickrFeed(data){
+	var count = 0;
+    flickrFeeds.feed = data.items.map(function( item ){
+    	item.count = count++;
+      return item;
+    });
+  drawFeed();
+}
 
 function drawFeed(){
 	var fplace = document.getElementById('fplace');
+	fplace.innerHTML = '';
 	var row = document.createElement('div');
   row.className = 'row';
 	flickrFeeds.feed
+		.sort(compare(filter))
 		.forEach(function(item, index){
+			var sourceSquare = (item.media.m).replace("_m.jpg", "_q.jpg");
 			var col = document.createElement('div');
-  		col.className = 'four columns';
+  		col.className = 'three columns';
 			col.innerHTML = '\
-					<article class="picture-item"> \
-            <div class="image">\
-              <img src="' + item.media.m + '" alt="picture">\
-            </div>\
-            <div class="img-text"> \
-              <h5><strong>' + item.title + '</strong></h5> \
-              <p><strong>Date: </strong>' + item.date_taken + '</p> \
-            </div> \
-          </article>';
-          row.appendChild(col);
-          if ((index + 1) % 3 === 0) {
-          fplace.appendChild(row);
-          row = document.createElement('div');
-          row.className = 'row';
-        } 
+					<a href='+ item.link + '>\
+						<article class="picture-item"> \
+	            <div class="image">\
+	              <img src="' + sourceSquare + '" alt="picture">\
+	            </div>\
+	            <div class="img-text"> \
+	              <h5><strong>' + item.title + '</strong></h5> \
+	              <p><strong>Date: </strong>' + item.date_taken + '</p> \
+	            </div> \
+	          </article>\
+	        </a>';
+      row.appendChild(col);
+      if ((index + 1) % 4 === 0) {
+      fplace.appendChild(row);
+      row = document.createElement('div');
+      row.className = 'row';
+    } 
 	});
-		if(row.children.length !== 0) {
-			fplace.appendChild(row);
-		}
+
+	if(row.children.length !== 0) {
+		fplace.replaceChild(row, fplace.childNodes[0][0]);
+	}
 }
+
+
+var feedtags = document.getElementById('feedtags');
+feedtags.addEventListener('change', function(){
+	var url;
+		if (!feedtags.value) {
+			url = flickrFeeds.flickrOptions.url;
+		}else {
+		var pos = flickrFeeds.flickrOptions.url.indexOf('&tags=')
+		url = flickrFeeds.flickrOptions.url.slice(0, pos) + '&tags=' + feedtags.value;
+		}
+		flickrFeeds.flickrOptions.url = url;
+		init();
+}, false);
+
+
+var filter;
+feedfilter.addEventListener('change', function(){
+	var feedfilter = document.getElementById('feedfilter');
+	filter = feedfilter.value;                                  //value equal date_taken or title
+	drawFeed();
+}, false);
+
+
+function compare(value) {
+  return function( a, b ){
+      if (a[value] < b[value]) return -1;
+      if (a[value] > b[value]) return 1;
+      return 0;
+    };
+} 
+
+function init(){
+	 	var scripts = document.head.getElementsByTagName('script');
+	 	if(scripts.length > 0){
+        document.head.removeChild(scripts[0]);
+     }
+    var head = document.getElementsByTagName('head')[0];
+    var addScript = document.createElement('script');
+    addScript.src = flickrFeeds.flickrOptions.url;
+    head.appendChild(addScript);
+    drawFeed();
+}
+
+init();
+
